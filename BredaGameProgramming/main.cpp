788b3cb5f18650020acaa4536dba2120834f6d50
Lurr1k <kkirilllirik@gmail.com>
2025-02-10 19:12:29 +0100

    Test comment

--- BredaGameProgramming/main.cpp
+++ BredaGameProgramming/main.cpp
@@ -18,7 +18,7 @@ int main()
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
-                window.close();
+                window.close(); 
             else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
                 
                 if (keyPressed->scancode == sf::Keyboard::Scancode::Escape){
