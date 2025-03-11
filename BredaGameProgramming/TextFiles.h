b2c276652a39043831e66e82f6dabaa7361f221d
Lurr1k <kkirilllirik@gmail.com>
2025-03-11 15:28:21 +0100

    newfile

---
+++ BredaGameProgramming/TextFiles.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <unordered_map>
+#include <fstream>
+
+class TextFiles {
+public:
+	static std::unordered_map<int, std::string> import_characters(std::string address) {
+
+
+
+	}
+
+};
